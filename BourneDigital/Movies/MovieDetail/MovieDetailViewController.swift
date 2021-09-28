a565ab51983d4e07cbf984c6ced8f582638355ba
AnkithaSreedhara <ankitha.sreedhara08@gmail.com>
2021-09-28 21:56:43 +1000

    Changed root storyboard and created MVC structure

---
+++ BourneDigital/Movies/MovieDetail/MovieDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailViewController.swift
+//  BourneDigital
+//
+//  Created by Sai Ankitha on 28/9/21.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ BourneDigital/Movies/Movies/MoviesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MoviesCollectionViewCell.swift
+//  BourneDigital
+//
+//  Created by Sai Ankitha on 28/9/21.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+}
